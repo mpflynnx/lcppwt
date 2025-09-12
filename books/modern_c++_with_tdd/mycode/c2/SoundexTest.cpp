0483b69b9e3075cabdd4aa041f53b8993b5b3617
mpflynnx <mpflynnx@gmail.com>
2025-09-12 10:55:02 +0000

    Refactor Soundex header file created

--- books/modern_c++_with_tdd/mycode/c2/SoundexTest.cpp
+++ books/modern_c++_with_tdd/mycode/c2/SoundexTest.cpp
@@ -1,12 +1,6 @@
-#include <string>
-
-class Soundex {
-
-public:
-  std::string encode(const std::string &word) const { return word + "000"; }
-};
-
+#include "Soundex.h"
 #include "gmock/gmock.h"
+
 using ::testing::Eq;
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
