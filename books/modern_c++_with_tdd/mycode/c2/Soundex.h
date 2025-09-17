7d9b38231732c0b876963e5ff4e1845fe2a85f9e
mpflynnx <mpflynnx@gmail.com>
2025-09-17 14:55:53 +0000

    Updated Soundex Class private member function: zeroPad()
    Removed magic literal 4, replaced by a named constant
    
    Tests Status:
    PASS RetainSoleLetterOfOneLetterWord
    PASS PadsWithZerosToEnsureThreeDigits
    FAIL ReplacesConsonantsWithAppropriateDigits

--- books/modern_c++_with_tdd/mycode/c2/Soundex.h
+++ books/modern_c++_with_tdd/mycode/c2/Soundex.h
@@ -5,13 +5,15 @@
 
 class Soundex {
  public:
+  static const size_t MaxCodeLength{4};
+
   std::string encode(const std::string &word) const { return zeroPad(word); }
 
  private:
-  std::string head(const std::string &word) const { return word.substr(0,1); }
+  std::string head(const std::string &word) const { return word.substr(0, 1); }
 
   std::string zeroPad(const std::string &word) const {
-    auto ZerosRequired = 4 - word.size();
+    auto ZerosRequired = MaxCodeLength - word.size();
     return word + std::string(ZerosRequired, '0');
   }
 };
