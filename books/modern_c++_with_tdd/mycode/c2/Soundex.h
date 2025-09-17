eb8cc73a22aa712594a0fac62b733e61ced367c4
mpflynnx <mpflynnx@gmail.com>
2025-09-17 14:42:34 +0000

    Added Soundex Class private member function: head()
    Returns a new string containing index 0 of word
    
    Tests Status:
    PASS RetainSoleLetterOfOneLetterWord
    PASS PadsWithZerosToEnsureThreeDigits
    FAIL ReplacesConsonantsWithAppropriateDigits

--- books/modern_c++_with_tdd/mycode/c2/Soundex.h
+++ books/modern_c++_with_tdd/mycode/c2/Soundex.h
@@ -8,6 +8,8 @@ class Soundex {
   std::string encode(const std::string &word) const { return zeroPad(word); }
 
  private:
+  std::string head(const std::string &word) const { return word.substr(0,1); }
+
   std::string zeroPad(const std::string &word) const {
     auto ZerosRequired = 4 - word.size();
     return word + std::string(ZerosRequired, '0');
