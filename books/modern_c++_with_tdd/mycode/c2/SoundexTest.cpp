63c5510a3beb332735a764f8e46259e5d1b4e021
mpflynnx <mpflynnx@gmail.com>
2025-10-06 10:49:35 +0000

    Added test: UppercasesFirstLetter - Test fails not implementation

--- books/modern_c++_with_tdd/mycode/c2/SoundexTest.cpp
+++ books/modern_c++_with_tdd/mycode/c2/SoundexTest.cpp
@@ -53,3 +53,7 @@ TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
 TEST_F(SoundexEncoding, CombinesDuplicateEncodings) {
   ASSERT_THAT(soundex.encode("Abfcgdt"), Eq("A123"));
 }
+
+TEST_F(SoundexEncoding, UppercasesFirstLetter) {
+  ASSERT_THAT(soundex.encode("abcd"), Eq("A123"));
+}
