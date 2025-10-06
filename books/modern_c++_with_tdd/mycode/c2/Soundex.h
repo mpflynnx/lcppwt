900612ed135c390851f818425f7cca97ac1f5c12
mpflynnx <mpflynnx@gmail.com>
2025-10-06 11:01:59 +0000

    Added new Soundex class private member function upperFront() - Returns a std::string of the first/front character in uppercase. - Use static_cast<> to handle any EOF caused by failed read operations from .front()
    Refactor encode()
    - Refactor to use upperFront() on the head of the word.
    
    Test pass: UppercasesFirstLetter()

--- books/modern_c++_with_tdd/mycode/c2/Soundex.h
+++ books/modern_c++_with_tdd/mycode/c2/Soundex.h
@@ -9,10 +9,15 @@ class Soundex {
   static const size_t MaxCodeLength{4};
 
   std::string encode(const std::string &word) const {
-    return zeroPad(head(word) + encodedDigits(tail(word)));
+    return zeroPad(upperFront(head(word)) + encodedDigits(tail(word)));
   }
 
  private:
+  std::string upperFront(const std::string &string) const {
+    return std::string(
+        1, std::toupper(static_cast<unsigned char>(string.front())));
+  }
+
   std::string head(const std::string &word) const { return word.substr(0, 1); }
 
   std::string tail(const std::string &word) const { return word.substr(1); }
