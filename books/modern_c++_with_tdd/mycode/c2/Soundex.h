9a5f58d32b95e8f6882a673aa9da48fae991a6f8
mpflynnx <mpflynnx@gmail.com>
2025-10-10 14:34:39 +0000

    Added new Soundex Class private member functions:-
    - encodeHead()
    Calls function encodedDigit() with the first character of string word
    Appends returned string to encoding to variable named encoding.
    
    - encodeTail()
    Iterates the tail of word, calling function encodeLetter() with each
    character of word
    Checks for completion of encoding by called isComplete() with the
     encoding variable
    
    - encodeLetter()
    Call function encodedDigit() with the character passed to it by
    encodeTail(). Checks if digit is valid before appended to encoding
    variable.
    
    Updated Soundex Class private member function
    
    - encodedDigits()
    Updated to use the newly created private member
    functions, encodeHead() and encodeTail() to complete
    the encoding variable
    Returns encoding variable to caller function encode()
    
    All tests pass
    
    Completes #19

--- books/modern_c++_with_tdd/mycode/c2/Soundex.h
+++ books/modern_c++_with_tdd/mycode/c2/Soundex.h
@@ -28,16 +28,28 @@ class Soundex {
 
   std::string encodedDigits(const std::string &word) const {
     std::string encoding;
-    encoding += encodedDigit(word.front());
+    encodeHead(encoding, word);
+    encodeTail(encoding, word);
+    return encoding;
+  }
 
+  void encodeHead(std::string &encoding, const std::string &word) const {
+    encoding += encodedDigit(word.front());
+  }
+
+  void encodeTail(std::string &encoding, const std::string &word) const {
     for (auto letter : tail(word)) {
-      if (isComplete(encoding)) break;
-      auto digit = encodedDigit(letter);
-      if (digit != NotADigit && digit != lastDigit(encoding)) {
-        encoding += digit;
+      if (!isComplete(encoding)) {
+        encodeLetter(encoding, letter);
       }
     }
-    return encoding;
+  }
+
+  void encodeLetter(std::string &encoding, char letter) const {
+    auto digit = encodedDigit(letter);
+    if (digit != NotADigit && digit != lastDigit(encoding)) {
+      encoding += digit;
+    }
   }
 
   bool isComplete(const std::string &encoding) const {
